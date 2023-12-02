bb7c68ec98feedbcc8adc169f8f83d70486ad853
kody.low <kodylow7@gmail.com>
2023-12-02 15:35:08 -0800

    fix: cleanup

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,12 @@
-use std::{collections::HashSet, fmt, str::FromStr};
+use std::{fmt::Display, str::FromStr};
 
-use nostr::bitcoin::PublicKey;
 use serde::{de, Deserialize, Deserializer};
 
 pub fn empty_string_as_none<'de, D, T>(de: D) -> Result<Option<T>, D::Error>
 where
     D: Deserializer<'de>,
     T: FromStr,
-    T::Err: fmt::Display,
+    T::Err: Display,
 {
     let opt = Option::<String>::deserialize(de)?;
     match opt.as_deref() {
